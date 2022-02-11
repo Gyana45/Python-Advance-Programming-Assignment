b614a42e685ccc6b7cd3ce7dedbc99662ba7795e
Gyana45 <95517550+Gyana45@users.noreply.github.com>
2022-02-11 14:32:59 +0530

    Delete question_4.py

--- question_4.py
+++
@@ -1,6 +0,0 @@
-import re
-
-lst = ["bad cookie", "good cookie", "bad cookie", "good cookie", "good cookie"]
-pattern = "yourregularexpression bad cookie here"
-
-print(len(re.findall(pattern, ", ".join(lst))) )
